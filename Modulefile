65770fecdff3bcd0cc8dfb2b8b8c3770ce030ad8
Kris Buytaert <sdog@inuits.be>
2012-11-23 19:58:14 +0100

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'pagerduty'
+version '0.0.1'
+source 'UNKNOWN'
+author 'krisbuytaert'
+license 'GPLv2'
+summary 'UNKNOWN'
+description 'UNKNOWN'
+project_page 'UNKNOWN'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+# == Class: pagerduty
+#
+# This class will configure and install the pagerduty config for 
+#  -Icinga
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if it
+#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should not be used in preference to class parameters  as of
+#   Puppet 2.6.)
+#
+# === Examples
+#
+#  class { pagerduty:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  }
+#
+# === Authors
+#
+# Kris Buytaert <Kris.Buytaert@gmail.com>
+#
+#
+class pagerduty {
+
+
+}
